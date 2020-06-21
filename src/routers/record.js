6498951fa5a3ecd48f6b961f852c6273d047abbe
Buğrahan Memiş <bugrahan.memis@inomera.com>
2020-06-21 22:13:24 +0300

    Add request and response

--- src/routers/record.js
+++ src/routers/record.js
@@ -11,4 +11,45 @@ router.get('/records', async (req, res) => {
     })
 })
 
+router.post('/records', async (req, res) => {
+
+    const records = await Record.aggregate([
+        {
+            $project: {
+                key: '$key',
+                createdAt : '$createdAt',
+                totalNumber: {
+                    $sum: '$counts'
+                },
+                _id: 0
+            }
+        },
+        {
+            $match: {
+                createdAt: {
+                    $gte: new Date(req.body.startDate),
+                    $lte: new Date(req.body.endDate)
+                },
+                totalNumber: {
+                    $gte: req.body.minCount,
+                    $lte: req.body.maxCount
+                }
+            }
+            
+        }
+    ]).then((recordsData) => {
+        console.log(req.body)
+        console.log(recordsData)
+        res.status(200).send({
+            code: 1,
+            msg: 'Success',
+            records: recordsData
+        })
+    }).catch((e) => {
+        console.log(e)
+        res.status(400).send()
+    })
+
+})
+
 module.exports = router
